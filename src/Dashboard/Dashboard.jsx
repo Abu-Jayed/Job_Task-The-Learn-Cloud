5241ac52b5796094d8394cb8f0ab8f7831e98028
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-21 00:31:20 +0600

    successfully able to drag element.

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
-import { data } from "./data";
 import { useEffect, useState } from "react";
+import ReactDragListView from 'react-drag-listview/lib/index.js';
 
 const Dashboard = () => {
+  const [todos, setTodo] = useState([]);
 
-  const [todos, setTodo] = useState([])
-  
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:3000/`)
-    .then(res => res.json())
-    .then(data => {
-      setTodo(data)
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setTodo(data);
+      });
+  }, []);
+
+  const handleDragEnd = (fromIndex, toIndex) => {
+    const updatedData = [...todos];
+    const item = updatedData.splice(fromIndex, 1)[0];
+    updatedData.splice(toIndex, 0, item);
+    setTodo(updatedData);
+  };
 
   return (
     <>
@@ -20,37 +26,52 @@ const Dashboard = () => {
         <Link to={"/dashboard/add"}>Add Todos</Link>
       </button>
       <div></div>
-      <div className="rounded-md border-2 ">
-        <table className=" max-w-6xl min-w-full">
-          <thead className="bg-gray-50 ">
-            <tr className="text-xl">
-              <th className="p-3"> Todo</th>
-              <th>Status</th>
-              <th>Priority</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              todos.map(todo => {
-                return (
-                  <tr className="border-b" key={todo._id}>
-              <td className="p-2">{todo.title}</td>
-
-              <td className=" w-20 font-bold">
-                <select className={`${todo.status==="completed"?"text-green-600": "text-red-700"} w-24`} name="" id="">
-                  <option className="text-red-700 font-bold" value="Hello">{todo.status === "completed"? "completed": "uncompleted"}</option>
-                  <option className="text-green-700 font-bold" value="Hi">{todo.status === "completed"? "uncompleted": "completed"}</option>
-                </select>
-              </td>
-              <td>{todo.priority}</td>
-
-
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+      <div className="rounded-md border-2">
+        <ReactDragListView onDragEnd={handleDragEnd} nodeSelector="tr">
+          <table className="max-w-6xl min-w-full">
+            <thead className="bg-gray-50">
+              <tr className="text-xl">
+                <th className="p-3"></th>
+                <th className="p-3">Todo</th>
+                <th>Status</th>
+                <th>Priority</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todos.map((todo, i) => (
+                <tr className="border-b" key={i}>
+                  <p className="hover:cursor-pointer">Drag</p>
+                  <td className="p-2">{todo.title}</td>
+                  <td className="w-20 font-bold">
+                    <select
+                      className={`${todo.status === 'completed' ? 'text-green-600' : 'text-red-700'} w-24`}
+                      name=""
+                      id=""
+                    >
+                      <option
+                        className="text-red-700 font-bold"
+                        value="Hello"
+                      >
+                        {todo.status === 'completed'
+                          ? 'completed'
+                          : 'uncompleted'}
+                      </option>
+                      <option
+                        className="text-green-700 font-bold"
+                        value="Hi"
+                      >
+                        {todo.status === 'completed'
+                          ? 'uncompleted'
+                          : 'completed'}
+                      </option>
+                    </select>
+                  </td>
+                  <td>{todo.priority}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </ReactDragListView>
       </div>
     </>
   );
