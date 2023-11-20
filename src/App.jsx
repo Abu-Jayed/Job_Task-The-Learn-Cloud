88888a4f125982692bc7d135ae59ce96f825c1bf
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-20 21:10:25 +0600

    demo todo table created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
-import About from "./About/About";
 import "./App.css";
-import Dashboard from "./Dashboard/Dashboard";
 import NavBar from "./NavBar/NavBar";
 
 function App() {
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,10 +1,34 @@
-
 const Dashboard = () => {
   return (
-    <div>
-      <button className="p-2 bg-rose-400 rounded-md text-xl font-bold text-white">Add Todos</button>
-      <h1>This is our dashboard</h1>
-    </div>
+    <>
+      <button className="p-2 bg-rose-400 rounded-md text-xl font-bold text-white">
+        Add Todos
+      </button>
+      <div></div>
+      <div className="rounded-md border-2 ">
+        <table className=" max-w-6xl min-w-full">
+          <thead className="bg-gray-50 ">
+            <tr className="text-xl">
+              <th className="p-3"> Todo</th>
+              <th>Status</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b ">
+              <td className="p-2">Wake up</td>
+              <td className="text-green-700 font-bold">Completed</td>
+              <td>Sunday</td>
+            </tr>
+            <tr className="border-b">
+              <td>Exercise</td>
+              <td className="text-red-700 font-bold">unCompleted</td>
+              <td>Sunday</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
