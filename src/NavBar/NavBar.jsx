f4be2e72402afb68676b015e56bcd4e6950dc934
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-20 19:09:58 +0600

    basic navbar created.

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul className="flex gap-6 ">
+        <Link className="bg-gray-300 p-2 rounded-2xl px-3" to={"/"}>Home</Link>
+        <Link className="bg-gray-300 p-2 rounded-2xl px-3" to={"dashboard"}>Dashboard</Link>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
